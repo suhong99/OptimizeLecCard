113a2e9143931095167f29f7bba47db622edc29d
SUHONG\topic <topic828@Naver.com>
2023-12-25 07:29:34 +0900

    vscode오류 수정

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import { Suspense } from 'react'
+
+import Top from '@shared/Top'
+import AdBanners from '@components/home/AdBanners'
+import CardList from '@components/home/CardList'
+import ListRow from '@shared/ListRow'
+
+function HomePage() {
+  return (
+    <div>
+      <Top title="혜택 좋은 카드" subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요" />
+      <AdBanners />
+      <Suspense
+        fallback={[...new Array(10)].map((_, idx) => (
+          <ListRow.Skeleton key={idx} />
+        ))}
+      >
+        <CardList />
+      </Suspense>
+    </div>
+  )
+}
+
+export default HomePage
