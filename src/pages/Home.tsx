649d87f582eda09e6d0a0ad60f8106de7ffa526d
SUHONG\topic <topic828@Naver.com>
2023-12-25 06:00:05 +0900

    서스펙스 스켈레톤 이미지

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,22 @@
+import { Suspense } from 'react'
+
 import Top from '@shared/Top'
 import AdBanners from '@components/home/AdBanners'
 import CardList from '@components/home/CardList'
-// import ListRow from '@shared/ListRow'
+import ListRow from '@shared/ListRow'
 
 function HomePage() {
   return (
     <div>
       <Top title="혜택 좋은 카드" subTitle="회원님을 위해서 혜택 좋은 카드를 모아봤어요" />
       <AdBanners />
-      {/* <Suspense
+      <Suspense
         fallback={[...new Array(10)].map((_, idx) => (
           <ListRow.Skeleton key={idx} />
         ))}
       >
-      </Suspense> */}
-      <CardList />
+        <CardList />
+      </Suspense>
     </div>
   )
 }
